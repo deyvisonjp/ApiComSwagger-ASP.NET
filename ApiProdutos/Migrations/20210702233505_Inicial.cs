484acf64a68abdad033f697d9fd21abb17b52af0
Deyvison J. Paula <38982851+deyvisonjp@users.noreply.github.com>
2021-07-02 21:04:15 -0300

    Commit Inicial

---
+++ ApiProdutos/Migrations/20210702233505_Inicial.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ApiProdutos.Migrations
+{
+    public partial class Inicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Produtos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Nome = table.Column<string>(type: "TEXT", maxLength: 80, nullable: true),
+                    Preco = table.Column<decimal>(type: "TEXT", precision: 10, scale: 2, nullable: false),
+                    Estoque = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Produtos", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Produtos",
+                columns: new[] { "Id", "Estoque", "Nome", "Preco" },
+                values: new object[] { 1, 10, "Caderno", 7.95m });
+
+            migrationBuilder.InsertData(
+                table: "Produtos",
+                columns: new[] { "Id", "Estoque", "Nome", "Preco" },
+                values: new object[] { 2, 50, "Caneta", 1.5m });
+
+            migrationBuilder.InsertData(
+                table: "Produtos",
+                columns: new[] { "Id", "Estoque", "Nome", "Preco" },
+                values: new object[] { 3, 20, "Lapiseira", 2.80m });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Produtos");
+        }
+    }
+}
---
+++ ApiProdutos/README.md
@@ -0,0 +1,14 @@
+﻿# ASP.NET 5 - Criando uma Web API com ASP.NET Core 5, EF Core 5 e MySql 
+(Projeto do canal . . . Jose Carlos Macoratti)
+
+## Tencologias utilizadas
+- NET Core SDK 5.0
+- EF Core 5.0
+- SqLite
+- Swagger
+
+## Pacotes utilizados
+- Microsoft.EntityFrameworkCore
+- Microsoft.EntityFrameworkCore.Tools
+- Microsoft.EntityFrameworkCore.Design
+- Microsoft.EntityFrameworkCore.SqLite
